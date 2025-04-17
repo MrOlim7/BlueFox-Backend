1020d6a98e20373ef2648480ee642553759ffee9
OlimDev <mrolimyt@gmail.com>
2025-04-17 18:46:55 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -87,21 +87,6 @@ app.post('/signup', async (req, res) => {
   }
 });
 
-// Route login
-app.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Email ou mot de passe incorrect.' });
-    }
-
-    res.status(200).json({ message: 'Connexion réussie !' });
-  } catch (err) {
-    res.status(500).json({ message: 'Erreur lors de la connexion.' });
-  }
-});
 
 // Route login
 app.post('/login', async (req, res) => {
